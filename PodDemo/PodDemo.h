cbb0ea0a25e6cb1d442b49ed3b274cc98cbe3d3b
Aaron Douglas <astralbodies@gmail.com>
2015-11-05 07:12:11 -0600

    Initial Commit

---
+++ PodDemo/PodDemo.h
@@ -0,0 +1,19 @@
+//
+//  PodDemo.h
+//  PodDemo
+//
+//  Created by Aaron Douglas on 11/5/15.
+//  Copyright © 2015 Automattic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PodDemo.
+FOUNDATION_EXPORT double PodDemoVersionNumber;
+
+//! Project version string for PodDemo.
+FOUNDATION_EXPORT const unsigned char PodDemoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PodDemo/PublicHeader.h>
+
+
